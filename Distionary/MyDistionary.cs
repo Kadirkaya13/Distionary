4c27ce9a6a5bddedc73f7d9d1b350e1eb6558945
kadir kaya <kadirkaya.13@outlook.com.tr>
2021-01-29 18:08:40 +0300

    Add project files.

---
+++ Distionary/MyDistionary.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Distionary
+{
+    class MyDistionary<V,N>
+    {
+        V[] _arrayValue;
+        N[] _arrayNumber;
+        public MyDistionary()
+        {
+            _arrayValue = new V[0];
+            _arrayNumber = new N[0]; 
+
+        }
+        public void Add(V value ,N number )
+        {
+            //referansı kaybetmemek için geçiçi bir değişkene atama yapılır
+            V[] _tempValue = _arrayValue;
+            N[] _tempNumber = _arrayNumber;
+
+            _arrayValue = new V[_arrayValue.Length + 1];
+            _arrayNumber = new N[_arrayNumber.Length + 1];
+            for (int i = 0; i < _tempValue.Length; i++)
+            {
+                _arrayValue[i] = _tempValue[i];
+                _arrayValue[_arrayValue.Length - 1] = value;
+
+            }
+            for (int i = 0; i < _tempNumber.Length; i++)
+            {
+                _arrayNumber[i] = _tempNumber[i];
+                _arrayNumber[_arrayNumber.Length - 1] = number;
+
+            }
+            Console.WriteLine("Şehir Adı :"+value+" | "+" Plakası :"+number);
+        }
+
+    }
+}
---
+++ Distionary/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Distionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDistionary<string,string> myDistionary = new MyDistionary<string, string>();
+            myDistionary.Add("İstanbul","34");
+            myDistionary.Add("Samsun","55");
+            myDistionary.Add("Adana","01");
+            myDistionary.Add("Ankara","06");
+        }
+    }
+}
